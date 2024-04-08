8cd00d82a473263751f2628253ba3abbef924b9e
luanabrizola <130062674+luanabrizola@users.noreply.github.com>
2024-04-08 15:00:19 -0300

    Conhecendo e fazendo testes

---
+++ projeto_teste/src/app/page.tsx
@@ -0,0 +1,15 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="">
+      
+      <h1>Olá munndo</h1>
+       
+    
+
+ 
+ 
+    </main>
+  );
+}
